73b03bf6b68a4f6d71cf3e812fd579083cbfdbd7
Tom <44425373+tom-bakker@users.noreply.github.com>
2021-06-08 23:30:26 +1000

    Added comments and functions
    Changed the overall file to have functions and a main function for initial entry. Added comments using the Google Code Style.

--- Main.py
+++ Main.py
@@ -1,11 +1,24 @@
+"""Python project to webscrape particular news websites for articles.
+
+"""
 from bs4 import BeautifulSoup
 import requests
-URL = 'https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blacklight/'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-title = soup.find('h1')
-body = soup.find('div', class_='article-body')
-print(title.text)
-print(body.text)
+"""Fetches the article contents and title from a URL
+
+Args:
+URL : The URL of the article to webscrape.
+"""
+def get_article(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find('h1')
+    body = soup.find('div', class_='article-body')
+    print(title.text)
+    print(body.text)
+def main():
+    URL = 'https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blacklight/'
+    get_article(URL)
+if __name__ =="__main__":
+    main()
 
 
